910028d2ee973e74db07445069d136ba5cba0e97
Emily Munro <emilyclairemunro@icloud.com>
2024-03-03 22:11:38 +0000

    a bit more database progress

--- Cardiomegaly/measurement.py
+++ Cardiomegaly/measurement.py
@@ -1,6 +1,5 @@
 from point import Point
 import math
-from database import Database
 
 class Measurement:
 
@@ -16,50 +15,5 @@ class Measurement:
         self.start = Point(0.0, 0.0)
         self.end = Point(0.0, 0.0)
 
-    @staticmethod 
-    def initialise_database(database:Database):
-        create_users_table = """
-        CREATE TABLE IF NOT EXISTS image_measurements (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image_name TEXT NOT NULL,
-            heart_x INTEGER,
-            heart_y INTEGER,
-            thorax_x INTEGER,
-            thorax_y INTEGER
-        );
-        """
-        #right now only one point of x and y are being accounted for so i want to make a 2D array that 
-        #will create heart_start.x and heart_end.x then repeat this for y and thorax but if i use a 2D array 
-        #that means that it will still only be heart_start, heart_end, thorax_start, and thorax_end
-
-    @staticmethod
-    def from_database(database:Database):
-        all_results : List[Result] = []
-        select_users = "SELECT * from image_measurements"
-        print("select all measurements and create array")
-        return all_results
-    
-    def save(self, database:Database, all_results):
-        print("save the current measurements to database, if it does'nt exist insert and if it does update")
-        
-        if all_results.image_name == database:
-            sql = f"""
-            UPDATE
-              image_measurements
-            SET
-              heart_x = {all_results['heart_x']},
-              heart_y = {all_results['heart_y']},
-              thorax_x = {all_results['thorax_x']},
-              thorax_y = {all_results['thorax_y']}
-            WHERE
-              image_name = '{all_results['image_name']}'
-                """
-        else:
-            create_users = """
-            INSERT INTO
-              image_measurements(image_name, heart_x, heart_y thorax_x, thorax_y)
-            VALUES
-              (all_results.image_name, all_results.heart_x, all_results.heart_y, all_results.thorax_x, all_results.thorax_y);
-            """
 
 
--- Cardiomegaly/results.py
+++ Cardiomegaly/results.py
@@ -9,10 +9,6 @@ class Result:
         self.thorax = Measurement("Thorax")
         self.image_name = ""  
         self.short_image_name = ""
-        self.heart_x = 0
-        self.heart_y = 0
-        self.thorax_x = 0
-        self.thorax_y = 0
         self.pixel_spacing = [1.0, 1.0]   
         self.patient_ID = []
         self.patient_gender = []
@@ -28,3 +24,56 @@ class Result:
 
     def symptoms(self) -> bool:
         return self.ratio() > 0.5
+    
+    @staticmethod 
+    def initialise_database(database:Database):
+        create_users_table = """
+        CREATE TABLE IF NOT EXISTS image_measurements (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image_name TEXT NOT NULL,
+            heart_start_x INTEGER,
+            heart_start_y INTEGER,
+            heart_end_x INTEGER,
+            heart_end_y INTEGER,
+            thorax_start_x INTEGER,
+            thorax_start_y INTEGER,
+            thorax_end_x INTEGER,
+            thorax_end_y INTEGER
+        );
+        """
+        
+    @staticmethod
+    def from_database(database:Database):
+        all_results : List[Result] = []
+        select_users = "SELECT * from image_measurements"
+        Measurement = select_users
+        print("select all measurements and create array")
+        return all_results
+    
+    def save(self, database:Database, all_results):
+        print("save the current measurements to database, if it does'nt exist insert and if it does update")
+        
+        if self.image_name == database:
+            sql = f"""
+            UPDATE
+              image_measurements
+            SET
+              heart_start_x = {all_results['self.heart.start.x']},
+              heart_start_y = {all_results['self.heart.start.y']},
+              heart_end_x = {all_results['self.heart.end.x']},
+              heart_end_y = {all_results['self.heart.end.y']},
+              thorax_start_x = {all_results['self.thorax.start.x']},
+              thorax_start_y = {all_results['self.thorax.start.y']},
+              thorax_end_x = {all_results['self.thorax.end.x']},
+              thorax_end_y = {all_results['self.thorax.end.y']}
+            WHERE
+              image_name = '{all_results['self.image_name']}'
+                """
+        else:
+            create_users = """
+            INSERT INTO
+              image_measurements(image_name, heart_x, heart_y thorax_x, thorax_y)
+            VALUES
+              (self.image_name, self.heart.start.x, self.heart.start.y, self.heart.end.x, self.heart.end.y, self.thorax.start.x, self.thorax.start.y, self.thorax.end.x, self.thorax.end.y);
+            """
+
